33ac25dc0b1d61d5bd76cffeac4d43f4d224deab
mirsahib <mirsahib24@gmail.com>
2019-07-08 09:44:18 +0600

    classwork 8b

--- server.js
+++ server.js
@@ -3,19 +3,6 @@ var express = require('express');
 var app = express();
 var server = http.Server(app)
 
-  // var server = http.createServer(function(req, res){
-  //   res.statusCode = 200;
-  //   res.setHeader('Content-Type', 'text/html');
-  //   fs.readFile('index.html',function(err,data){
-  //     if(err){
-  //       return console.log("File not found")
-  //     }
-  //     res.end(data);
-  //   })
-  // });
-  // server.listen(3000, 'localhost', function(){
-  //   console.log('Server running');
-  // });
   app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html')
   })
